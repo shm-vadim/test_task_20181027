5770a353a377c3aa97d4fe9d179929af6a10b5ca
Vadim Shmakov <shmakovvadim@mail.ru>
2018-10-27 18:59:56 +0500

    Created Transaction entity

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TransactionRepository")
+ */
+class Transaction
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $addTime;
+
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    private $companyTicker;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="transactions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sharesCount;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $money;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isBought;
+
+    public function getId() : ? int
+    {
+        return $this->id;
+    }
+
+    public function getAddTime() : ? \DateTimeInterface
+    {
+        return $this->addTime;
+    }
+
+    public function setAddTime(\DateTimeInterface $addTime) : self
+    {
+        $this->addTime = $addTime;
+
+        return $this;
+    }
+
+    public function getCompanyTicker() : ? string
+    {
+        return $this->companyTicker;
+    }
+
+    public function setCompanyTicker(string $companyTicker) : self
+    {
+        $this->companyTicker = $companyTicker;
+
+        return $this;
+    }
+
+    public function getUser() : ? User
+    {
+        return $this->user;
+    }
+
+    public function setUser(? User $user) : self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getSharesCount() : ? int
+    {
+        return $this->sharesCount;
+    }
+
+    public function setSharesCount(int $sharesCount) : self
+    {
+        $this->sharesCount = $sharesCount;
+
+        return $this;
+    }
+
+    public function getMoney(): ?float
+    {
+        return $this->money;
+    }
+
+    public function setMoney(float $money): self
+    {
+        $this->money = $money;
+
+        return $this;
+    }
+
+    public function getIsBought(): ?bool
+    {
+        return $this->isBought;
+    }
+
+    public function setIsBought(bool $isBought): self
+    {
+        $this->isBought = $isBought;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
 
@@ -17,8 +19,51 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Transaction", mappedBy="user", orphanRemoval=true)
+     */
+    private $transactions;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->transactions = new ArrayCollection();
+    }
+
     public function getId() : ? int
     {
         return $this->id;
     }
+
+    /**
+     * @return Collection|Transaction[]
+     */
+    public function getTransactions(): Collection
+    {
+        return $this->transactions;
+    }
+
+    public function addTransaction(Transaction $transaction): self
+    {
+        if (!$this->transactions->contains($transaction)) {
+            $this->transactions[] = $transaction;
+            $transaction->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTransaction(Transaction $transaction): self
+    {
+        if ($this->transactions->contains($transaction)) {
+            $this->transactions->removeElement($transaction);
+            // set the owning side to null (unless already changed)
+            if ($transaction->getUser() === $this) {
+                $transaction->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
 }
